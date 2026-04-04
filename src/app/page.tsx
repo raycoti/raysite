29f378381de392d86c9aa753df41541465f70952
raycoti <raymond.coti@gmail.com>
2026-04-04 01:39:21 -0500

    fixes slide up styling, remove fade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,18 @@ import Link from 'next/link';
 import {Roboto, Nunito} from 'next/font/google';
 import {ViewTransition} from 'react';
 
-const nunito = Nunito({variable: '--font-nunito', subsets: ['latin']});
+const nunito = Nunito({variable: '--nunito', subsets: ['latin']});
 const roboto = Roboto({variable: '--roboto', subsets: ['latin']});
 
 export default function Home() {
   return (
-    <ViewTransition
-      default='none'
-      enter='slide-in'
-      exit='slide-out'
-      update='pulse'
-      name='home-splash'
-    >
+    <ViewTransition default='no-fade' enter='slide-in' exit='slide-out'>
       <div className='container text-center flex flex-col justify-center items-center relative opacity-100 min-w-full min-h-dvh'>
-        <div className='white w-full flex flex-col items-center relative justify-center py-6 max-w-4xl'>
-          <h1 className={nunito.className}>Ray Coti</h1>
-          <h2 className={nunito.className}>Blurring the line between dreams and reality</h2>
+        <div className='white w-full flex flex-col items-center relative justify-center py-7 max-w-4xl'>
+          <h1 className={`${nunito.className} font-bold text-6xl my-5`}>Ray Coti</h1>
+          <h2 className={`${nunito.className} font-bold text-3xl my-5`}>
+            Blurring the line between dreams and reality
+          </h2>
           <Link className={`enter ${roboto.variable}`} href='/about'>
             ENTER
           </Link>
